91e67c78229fec4ba90737c702cf7ce99a26a0c3
U-Skynote\Администратор <@Skynote.(none)>
2011-06-30 10:56:16 +0600

    Начало разработки

---
+++ main_module.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+'''
+Created on 29.06.2011
+
+@author: Моисеев Данил
+'''
+
+import sqlite3
+import VideoCapture
+
+if __name__ == '__main__':
+    pass
---
+++ model.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+
+'''
+Created on 30.06.2011
+
+@author: Моисеев Данил
+'''
+
+class Database(object):
+    '''
+    Класс предназначен для взаимодействия с базой
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
