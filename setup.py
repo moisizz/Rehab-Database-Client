59bd3c244b610b570f38978110b2c5e24e990c12
U-Skynote\Администратор <@Skynote.(none)>
2011-07-07 19:55:36 +0600

    Обновление скрипта сборки

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 import sys
 import py2exe
 
-setup(windows=['main_module.py'], options = { "py2exe":{"dll_excludes":["MSVCP90.dll"], "includes" : ["sip"]}})
+setup(windows=['main_module.py'], options = { "py2exe":{"dll_excludes":["MSVCP90.dll"], "includes" : ["sip","sqlite3","sqlalchemy.dialects.sqlite"]}})
